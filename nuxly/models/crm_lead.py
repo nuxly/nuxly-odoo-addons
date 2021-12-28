031423a724ec526e038c8007cbd982331303d093
mahenthirarajah <mahenthirarajahk@gmail.com>
2021-12-28 10:47:17 +0100

    réorganization du repository

--- nuxly/models/crm_lead.py
+++
@@ -1,9 +0,0 @@
-import logging
-from odoo import models, fields, api, _
-
-_logger = logging.getLogger(__name__)
-
-
-class crm_lead(models.Model):
-    _inherit = 'crm.lead'
-    category_id = fields.Char(string="Suivi GDoc", translate=True, required=False, help='Lien du suivi Google Doc')
--- nuxly/models/crm_lead_suivi_gdoc.py
+++
@@ -1,9 +0,0 @@
-import logging
-from odoo import models, fields
-
-_logger = logging.getLogger(__name__)
-
-
-class crmLeadSuiviGDoc(models.Model):
-    _inherit = 'crm.lead'
-    gdoc_url = fields.Char(string="Suivi GDoc", translate=True, required=False, help='Lien du suivi Google Doc')
