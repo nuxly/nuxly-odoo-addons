d32c7406cb00a4a26760dbd39ab4fc2f984849b3
mahenthirarajah <kmahenthirarajah@nuxly.com>
2022-07-28 17:35:15 +0200

    #61 correction of processus of send mail

--- timesheet_reminder_mail/models/reminder.py
+++ timesheet_reminder_mail/models/reminder.py
@@ -10,43 +10,50 @@ class Reminder(models.TransientModel):
     _description = "Alert each user that doesn't wrote at least one line of time on timesheet"
 
     def _cron_reminder(self):
-        publicHoliday = self.env['hr.holidays.public.line']
+        public_holiday = self.env['hr.holidays.public.line']
         leave = self.env['hr.leave']
         timesheet = self.env['account.analytic.line']
-        hrEmployee = self.env['hr.employee']
+        hr_employee = self.env['hr.employee']
 
         today = date.today()
         today_time = datetime.now()
 
         # Checking working day (not weekend)
-        if today.weekday() in [0,1,2,3,4]:
+        if today.weekday() in [0, 1, 2, 3, 4]:
             # checking if day is a holiday
-            holiday = publicHoliday.search([]).filtered(lambda x: x.date == today)
+            holiday = public_holiday.search([]).filtered(lambda x: x.date == today)
             if not holiday:
                 # Check for all employees, if each one write at least one line of timesheet
-                employees = hrEmployee.search([])
+                employees = hr_employee.search([])
                 for employee in employees:
-                    if employee.id != 8:
-                        continue
                     # Check if each one doesn't on leave
-                    onLeave = leave.search([('employee_id', '=', employee.id)]).filtered(lambda
+                    on_leave = leave.search([('employee_id', '=', employee.id)]).filtered(lambda
                                                                                              x: x.number_of_days == 1.0 and x.date_from == today_time or x.date_from <= today_time <= x.date_to)
-                    if not onLeave:
+                    if not on_leave:
                         # Check if each one write at least one line of timesheet, or do a alert
                         lines = timesheet.search([('date', '=', today), ('employee_id', '=', employee.id)])
                         if not lines:
-                            # TODO a faire un rappel pour saisir les temps
-                            template_obj = self.env.ref('timesheet_reminder_mail.reminder_timesheet_fill')
-                            if template_obj:
-                                receipt_list = [employee.work_email]
+                            self._cron_timesheet_send_reminder(
+                                employee,
+                                'timesheet_reminder_mail.reminder_timesheet_fill',
+                                'hr.open_view_employee_list_my'
+                            )
 
-                                body = template_obj.body_html
-
-                                mail_values = {
-                                    'subject': template_obj.subject,
-                                    'body_html': body,
-                                    'email_to': ';'.join(map(lambda x: x, receipt_list)),
-                                    'email_from': template_obj.email_from,
-                                }
-                                stat_send = self.env['mail.mail'].create(mail_values).send()
-                                _logger.debug("Timesheet Reminder Mail ==> mail sent status to reminder to fill those time on timesheet '%s'.", stat_send)
+    def _cron_timesheet_send_reminder(self, employees, template_xmlid, action_xmlid, additionnal_values=None):
+        """ Send the email reminder to specified users
+            :param user_ids : list of user identifier to send the reminder
+            :param template_xmlid : xml id of the reminder mail template
+        """
+        action_url = '%s/web#menu_id=%s&action=%s' % (
+            self.env['ir.config_parameter'].sudo().get_param('web.base.url'),
+            self.env.ref('hr.open_view_employee_list_my').id,
+            self.env.ref(action_xmlid).id,
+        )
+        # send mail template to users having email address
+        template = self.env.ref(template_xmlid)
+        template_ctx = {'action_url': action_url}
+        if additionnal_values:
+            template_ctx.update(additionnal_values)
+        for employee in employees:
+            template.with_context(**template_ctx).send_mail(employee.id)
+            _logger.info("A reminder sent to user '%s'.", employee.name)
