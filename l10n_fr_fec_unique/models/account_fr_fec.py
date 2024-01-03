3eae4ca7ab2c8ba20141af008550b9faf5acb611
mlateb <mlateb@nycly.com>
2024-01-03 17:31:56 +0100

    [ADD] #4 Update function generate FEC file unique

--- l10n_fr_fec_unique/models/account_fr_fec.py
+++ l10n_fr_fec_unique/models/account_fr_fec.py
@@ -55,19 +55,12 @@ class AccountFrFecUnique(models.TransientModel):
         self.ensure_one()
         if not (self.env.is_admin() or self.env.user.has_group('account.group_account_user')):
             raise AccessDenied()
-        # We choose to implement the flat file instead of the XML
-        # file for 2 reasons :
-        # 1) the XSD file impose to have the label on the account.move
-        # but Odoo has the label on the account.move.line, so that's a
-        # problem !
-        # 2) CSV files are easier to read/use for a regular accountant.
-        # So it will be easier for the accountant to check the file before
-        # sending it to the fiscal administration
+
+        # If the 'Unique export' is not checked, the function keeps its basic operation
+        if not self.unique_export:
+            return super(AccountFrFecUnique, self).generate_fec()
+            
         
-        #######################
-        # PATCH STARTS HERE   #
-        #######################
- 
         # Management of start date and end date:
         # Initialize 'date_from' to January 1, 1900, if no start date is specified and unique export is enabled.
         if not self.date_from and self.unique_export:
@@ -84,11 +77,6 @@ class AccountFrFecUnique(models.TransientModel):
             raise UserError(_('You could not set the start date or the end date in the future.'))
         if self.date_from >= self.date_to:
             raise UserError(_('The start date must be inferior to the end date.'))
-
-                 
-        #######################
-        # PATCH ENDS HERE     #
-        #######################       
 
 
         company = self.env.company
@@ -114,172 +102,143 @@ class AccountFrFecUnique(models.TransientModel):
             u'Montantdevise',  # 16
             u'Idevise',        # 17
             ]
+
+        rows_to_write = [header]
         
-
-        #######################
-        # PATCH STARTS HERE   #
-        #######################
-        if self.unique_export:
-            rows_to_write = [header]
-            # INITIAL BALANCE
-            unaffected_earnings_account = self.env['account.account'].search([
-                ('account_type', '=', 'equity_unaffected'),
-                ('company_id', '=', company.id)
-            ], limit=1)
-
-            if self.pool['account.account'].name.translate:
-                lang = self.env.user.lang or get_lang(self.env).code
-                aa_name = f"COALESCE(aa.name->>'{lang}', aa.name->>'en_US')"
-            else:
-                aa_name = "aa.name"
-            
-            for row in self._cr.fetchall():
-                listrow = list(row)
-                account_id = listrow.pop()
-                rows_to_write.append(listrow)
-
-            # LINES
-            if self.pool['account.journal'].name.translate:
-                lang = self.env.user.lang or get_lang(self.env).code
-                aj_name = f"COALESCE(aj.name->>'{lang}', aj.name->>'en_US')"
-            else:
-                aj_name = "aj.name"
-
-            query_limit = int(self.env['ir.config_parameter'].sudo().get_param('l10n_fr_fec.batch_size', 500000)) # To prevent memory errors when fetching the results
-            
-            # Add to the sql request condition for unique export: filter out entries that have been already exported
-            sql_query = f'''
-            SELECT
-                REGEXP_REPLACE(replace(aj.code, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS JournalCode,
-                REGEXP_REPLACE(replace({aj_name}, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS JournalLib,
-                REGEXP_REPLACE(replace(am.name, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS EcritureNum,
-                TO_CHAR(am.date, 'YYYYMMDD') AS EcritureDate,
-                aa.code AS CompteNum,
-                REGEXP_REPLACE(replace({aa_name}, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS CompteLib,
-                CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable')
-                THEN
-                    CASE WHEN rp.ref IS null OR rp.ref = ''
-                    THEN rp.id::text
-                    ELSE replace(rp.ref, '|', '/')
-                    END
-                ELSE ''
-                END
-                AS CompAuxNum,
-                CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable')
-                THEN COALESCE(REGEXP_REPLACE(replace(rp.name, '|', '/'), '[\\t\\r\\n]', ' ', 'g'), '')
-                ELSE ''
-                END AS CompAuxLib,
-                CASE WHEN am.ref IS null OR am.ref = ''
-                THEN '-'
-                ELSE REGEXP_REPLACE(replace(am.ref, '|', '/'), '[\\t\\r\\n]', ' ', 'g')
-                END
-                AS PieceRef,
-                TO_CHAR(COALESCE(am.invoice_date, am.date), 'YYYYMMDD') AS PieceDate,
-                CASE WHEN aml.name IS NULL OR aml.name = '' THEN '/'
-                    WHEN aml.name SIMILAR TO '[\\t|\\s|\\n]*' THEN '/'
-                    ELSE REGEXP_REPLACE(replace(aml.name, '|', '/'), '[\\t\\n\\r]', ' ', 'g') END AS EcritureLib,
-                replace(CASE WHEN aml.debit = 0 THEN '0,00' ELSE to_char(aml.debit, '000000000000000D99') END, '.', ',') AS Debit,
-                replace(CASE WHEN aml.credit = 0 THEN '0,00' ELSE to_char(aml.credit, '000000000000000D99') END, '.', ',') AS Credit,
-                CASE WHEN rec.name IS NULL THEN '' ELSE rec.name END AS EcritureLet,
-                CASE WHEN aml.full_reconcile_id IS NULL THEN '' ELSE TO_CHAR(rec.create_date, 'YYYYMMDD') END AS DateLet,
-                TO_CHAR(am.date, 'YYYYMMDD') AS ValidDate,
-                CASE
-                    WHEN aml.amount_currency IS NULL OR aml.amount_currency = 0 THEN ''
-                    ELSE replace(to_char(aml.amount_currency, '000000000000000D99'), '.', ',')
-                END AS Montantdevise,
-                CASE WHEN aml.currency_id IS NULL THEN '' ELSE rc.name END AS Idevise
-            FROM
-                account_move_line aml
-                LEFT JOIN account_move am ON am.id=aml.move_id
-                LEFT JOIN res_partner rp ON rp.id=aml.partner_id
-                JOIN account_journal aj ON aj.id = am.journal_id
-                JOIN account_account aa ON aa.id = aml.account_id
-                LEFT JOIN res_currency rc ON rc.id = aml.currency_id
-                LEFT JOIN account_full_reconcile rec ON rec.id = aml.full_reconcile_id
-            WHERE
-                am.date >= %s
-                AND am.date <= %s
-                AND am.company_id = %s
-                {"AND am.state = 'posted'" if self.export_type == 'official' else ""}
-                {"AND am.exported_date IS NULL" if self.unique_export else ""}
-            ORDER BY
-                am.date,
-                am.name,
-                aml.id
-            LIMIT %s
-            OFFSET %s
-            '''
-            #######################
-            # PATCH ENDs HERE     #
-            #######################
-
-            with io.BytesIO() as fecfile:
-                csv_writer = pycompat.csv_writer(fecfile, delimiter='|', lineterminator='')
-
-                # Write header and initial balances
-                for initial_row in rows_to_write:
-                    initial_row = list(initial_row)
-                    # We don't skip \n at then end of the file if there are only initial balances, for simplicity. An empty period export shouldn't happen IRL.
-                    initial_row[-1] += u'\r\n'
-                    csv_writer.writerow(initial_row)
-
-                # Write current period's data
-                query_offset = 0
-                has_more_results = True
-                while has_more_results:
-                    self._cr.execute(
-                        sql_query,
-                        (self.date_from, self.date_to, company.id, query_limit + 1, query_offset)
-                    )
-                    query_offset += query_limit
-                    has_more_results = self._cr.rowcount > query_limit # we load one more result than the limit to check if there is more
-                    query_results = self._cr.fetchall()
-                    for i, row in enumerate(query_results[:query_limit]):
-                        if i < len(query_results) - 1:
-                            # The file is not allowed to end with an empty line, so we can't use lineterminator on the writer
-                            row = list(row)
-                            row[-1] += u'\r\n'
-                        csv_writer.writerow(row)
-
-                base64_result = base64.encodebytes(fecfile.getvalue())
-
-            # Check if the query results are empty
-            if not query_results:
-                raise UserError(_('No data to export. The FEC file will not be generated.'))
-            
-            end_date = fields.Date.to_string(self.date_to).replace('-', '')
-            suffix = ''
-            if self.export_type == "nonofficial":
-                suffix = '-NONOFFICIAL'
-
-            self.write({
-                'fec_data': base64_result,
-                # Filename = <siren>FECYYYYMMDD where YYYMMDD is the closing date
-                'filename': '%sFEC%s%s.csv' % (company_legal_data, end_date, suffix),
-                })
-
-            # Set fiscal year lock date to the end date (not in test)
-            fiscalyear_lock_date = self.env.company.fiscalyear_lock_date
-            if not self.test_file and (not fiscalyear_lock_date or fiscalyear_lock_date < self.date_to):
-                self.env.company.write({'fiscalyear_lock_date': self.date_to})
-            
-            #######################
-            # PATCH STARTS HERE   #
-            #######################
-            
-            # Mark as exported all accounting entries that have been exported
-            self.mark_exported_unique()
-
-            #######################
-            # PATCH ENDS HERE     #
-            #######################
+        if self.pool['account.account'].name.translate:
+            lang = self.env.user.lang or get_lang(self.env).code
+            aa_name = f"COALESCE(aa.name->>'{lang}', aa.name->>'en_US')"
+        else:
+            aa_name = "aa.name"
         
-            return {
-                'name': 'FEC',
-                'type': 'ir.actions.act_url',
-                'url': "web/content/?model=account.fr.fec&id=" + str(self.id) + "&filename_field=filename&field=fec_data&download=true&filename=" + self.filename,
-                'target': 'self',
-            }
-        else: 
-            return super(AccountFrFecUnique, self).generate_fec()
+        for row in self._cr.fetchall():
+            listrow = list(row)
+            account_id = listrow.pop()
+            rows_to_write.append(listrow)
+
+        # LINES
+        if self.pool['account.journal'].name.translate:
+            lang = self.env.user.lang or get_lang(self.env).code
+            aj_name = f"COALESCE(aj.name->>'{lang}', aj.name->>'en_US')"
+        else:
+            aj_name = "aj.name"
+
+        query_limit = int(self.env['ir.config_parameter'].sudo().get_param('l10n_fr_fec.batch_size', 500000)) # To prevent memory errors when fetching the results
+        
+        # Add to the sql request condition for unique export: filter out entries that have been already exported
+        sql_query = f'''
+        SELECT
+            REGEXP_REPLACE(replace(aj.code, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS JournalCode,
+            REGEXP_REPLACE(replace({aj_name}, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS JournalLib,
+            REGEXP_REPLACE(replace(am.name, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS EcritureNum,
+            TO_CHAR(am.date, 'YYYYMMDD') AS EcritureDate,
+            aa.code AS CompteNum,
+            REGEXP_REPLACE(replace({aa_name}, '|', '/'), '[\\t\\r\\n]', ' ', 'g') AS CompteLib,
+            CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable')
+            THEN
+                CASE WHEN rp.ref IS null OR rp.ref = ''
+                THEN rp.id::text
+                ELSE replace(rp.ref, '|', '/')
+                END
+            ELSE ''
+            END
+            AS CompAuxNum,
+            CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable')
+            THEN COALESCE(REGEXP_REPLACE(replace(rp.name, '|', '/'), '[\\t\\r\\n]', ' ', 'g'), '')
+            ELSE ''
+            END AS CompAuxLib,
+            CASE WHEN am.ref IS null OR am.ref = ''
+            THEN '-'
+            ELSE REGEXP_REPLACE(replace(am.ref, '|', '/'), '[\\t\\r\\n]', ' ', 'g')
+            END
+            AS PieceRef,
+            TO_CHAR(COALESCE(am.invoice_date, am.date), 'YYYYMMDD') AS PieceDate,
+            CASE WHEN aml.name IS NULL OR aml.name = '' THEN '/'
+                WHEN aml.name SIMILAR TO '[\\t|\\s|\\n]*' THEN '/'
+                ELSE REGEXP_REPLACE(replace(aml.name, '|', '/'), '[\\t\\n\\r]', ' ', 'g') END AS EcritureLib,
+            replace(CASE WHEN aml.debit = 0 THEN '0,00' ELSE to_char(aml.debit, '000000000000000D99') END, '.', ',') AS Debit,
+            replace(CASE WHEN aml.credit = 0 THEN '0,00' ELSE to_char(aml.credit, '000000000000000D99') END, '.', ',') AS Credit,
+            CASE WHEN rec.name IS NULL THEN '' ELSE rec.name END AS EcritureLet,
+            CASE WHEN aml.full_reconcile_id IS NULL THEN '' ELSE TO_CHAR(rec.create_date, 'YYYYMMDD') END AS DateLet,
+            TO_CHAR(am.date, 'YYYYMMDD') AS ValidDate,
+            CASE
+                WHEN aml.amount_currency IS NULL OR aml.amount_currency = 0 THEN ''
+                ELSE replace(to_char(aml.amount_currency, '000000000000000D99'), '.', ',')
+            END AS Montantdevise,
+            CASE WHEN aml.currency_id IS NULL THEN '' ELSE rc.name END AS Idevise
+        FROM
+            account_move_line aml
+            LEFT JOIN account_move am ON am.id=aml.move_id
+            LEFT JOIN res_partner rp ON rp.id=aml.partner_id
+            JOIN account_journal aj ON aj.id = am.journal_id
+            JOIN account_account aa ON aa.id = aml.account_id
+            LEFT JOIN res_currency rc ON rc.id = aml.currency_id
+            LEFT JOIN account_full_reconcile rec ON rec.id = aml.full_reconcile_id
+        WHERE
+            am.date >= %s
+            AND am.date <= %s
+            AND am.company_id = %s
+            {"AND am.state = 'posted'" if self.export_type == 'official' else ""}
+            {"AND am.exported_date IS NULL" if self.unique_export else ""}
+        ORDER BY
+            am.date,
+            am.name,
+            aml.id
+        LIMIT %s
+        OFFSET %s
+        '''
+        
+        with io.BytesIO() as fecfile:
+            csv_writer = pycompat.csv_writer(fecfile, delimiter='|', lineterminator='')
+
+            # Write header and initial balances
+            for initial_row in rows_to_write:
+                initial_row = list(initial_row)
+                # We don't skip \n at then end of the file if there are only initial balances, for simplicity. An empty period export shouldn't happen IRL.
+                initial_row[-1] += u'\r\n'
+                csv_writer.writerow(initial_row)
+
+            # Write current period's data
+            query_offset = 0
+            has_more_results = True
+            while has_more_results:
+                self._cr.execute(
+                    sql_query,
+                    (self.date_from, self.date_to, company.id, query_limit + 1, query_offset)
+                )
+                query_offset += query_limit
+                has_more_results = self._cr.rowcount > query_limit # we load one more result than the limit to check if there is more
+                query_results = self._cr.fetchall()
+                for i, row in enumerate(query_results[:query_limit]):
+                    if i < len(query_results) - 1:
+                        # The file is not allowed to end with an empty line, so we can't use lineterminator on the writer
+                        row = list(row)
+                        row[-1] += u'\r\n'
+                    csv_writer.writerow(row)
+
+            base64_result = base64.encodebytes(fecfile.getvalue())
+
+        # Check if the query results are empty, dont generate the FEC file
+        if not query_results:
+            raise UserError(_('The file is empty, all the entries have already been exported'))
+        
+        end_date = fields.Date.to_string(self.date_to).replace('-', '')
+        suffix = ''
+        if self.export_type == "nonofficial":
+            suffix = '-TEST'
+
+        self.write({
+            'fec_data': base64_result,
+            # Filename = <siren>FECYYYYMMDD where YYYMMDD is the closing date
+            'filename': '%sFEC%s%s.csv' % (company_legal_data, end_date, suffix),
+            })
+        
+        # Mark as exported all accounting entries that have been exported if is not a test file
+        self.mark_exported_unique()
     
+        return {
+            'name': 'FEC',
+            'type': 'ir.actions.act_url',
+            'url': "web/content/?model=account.fr.fec&id=" + str(self.id) + "&filename_field=filename&field=fec_data&download=true&filename=" + self.filename,
+            'target': 'self',
+        }
