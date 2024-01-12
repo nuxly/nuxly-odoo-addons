ca5381c6b79f7285c38383ce9c03bfc015dc1e82
mlateb <mlateb@nycly.com>
2024-01-12 13:31:35 +0100

    [wip] Add comparaison with all partners

--- partner_name_unique/models/res_partner.py
+++ partner_name_unique/models/res_partner.py
@@ -1,5 +1,7 @@
 from odoo import fields, api, models
 from unidecode import unidecode
+import logging
+logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -14,7 +16,7 @@ class ResPartner(models.Model):
     )
 
     # Compute function that return the same_name_partner if exists
-    @api.depends('name', 'company_id')
+    @api.depends('name', 'company_id', 'company_registry')
     def _compute_same_name_partner_id(self):
         for partner in self:
             # use _origin to deal with onchange()
@@ -22,14 +24,25 @@ class ResPartner(models.Model):
             #active_test = False because if a partner has been deactivated you still want to raise the error,
             #so that you can reactivate it instead of creating a new one, which would loose its history.
             Partner = self.with_context(active_test=False).sudo()
-            if partner.name:
-                #add specific standards for names to take into specific accents and lower upper letters
-                name_normalized = unidecode(partner.name.strip().lower())
-                domain = [
-                    ('name', '=', name_normalized),
-                ]
-                if partner.company_id:
-                    domain += [('company_id', 'in', [False, partner.company_id.id])]
-                if partner_id:
-                    domain += ['!', ('id', 'child_of', partner_id)]
-                partner.z_same_name_partner_id = not partner.parent_id and Partner.search(domain, limit=1)
+
+            # Normalize the name for accents and convert to lowercase
+            name_normalized = unidecode(partner.name.strip()).lower()
+            logger.warning("Nom normalisé 1 {}".format(name_normalized))
+
+            # Prepare the search domain
+            domain = []
+            if partner.company_id:
+                domain += [('company_id', 'in', [False, partner.company_id.id])]
+            if partner_id:
+                domain += ['!', ('id', 'child_of', partner_id)]
+
+            # Search for all partners
+            all_partners = Partner.search(domain)
+
+            # Filter to find a partner with the same normalized name
+            partner.z_same_name_partner_id = False
+            for p in all_partners:
+                if unidecode(p.name.strip()).lower() == name_normalized and p.id != partner_id:
+                    logger.warning("Nom normalisé 2 {}".format(unidecode(p.name.strip()).lower()))
+                    partner.z_same_name_partner_id = p.id
+                    break
