9fbe9d9197fc3861515e32c02adae344da2cab43
mlateb <mlateb@nycly.com>
2024-01-12 09:51:32 +0100

    Update README + Initialization module partner_name_unique

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# l10n_fr_fec_unique
-
-## Overview
-The `l10n_fr_fec_unique` module is an extension for the Odoo module `l10n_fr_fec` that introduces a unique export feature for the FEC (Fichier des Écritures Comptables) file generation. It enhances the standard Odoo FEC generation process by allowing users to export only entries that have been already exported
-
-
 ## Modules & versions
 
 | Addon                    | Version     | Summary                              |
---
+++ partner_name_unique/__manifest__.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'partner_name_unique',
+    'version': '16.0.0.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Unique contact name',
+    'description': """
+    This module find partners with duplicate names add a warning.
+======================================================================
+
+For any question, send us an email to info@nuxly.com
+
+Features:
+-----------------------------------------------------------
+    """,
+    'author': 'Nuxly',
+    'website': 'https://www.nuxly.com',
+    'depends': [],
+    'data': [
+        # 'views/',
+    ],
+    'installable': True,
+}
