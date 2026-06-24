ced4e4c969088fc1ccad4da8b405f73660e9a93f
mlateb <mlateb@nuxly.com>
2026-06-24 11:38:23 +0200

    #18 [IMP] Use product.product for mileage expense identification

--- hr_expense_ma/__init__.py
+++ hr_expense_ma/__init__.py
@@ -1,2 +1,3 @@
 # Copyright 2026 Nuxly
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import models
--- hr_expense_ma/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    is_ik_expense = fields.Boolean(string="Mileage allowance", tracking=True, help="Enable this option to identify this expense product as a mileage allowance product.",)
