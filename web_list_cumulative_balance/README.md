6092cf70cf0dba4bf01dddf9a5f89b4075d47edc
mlateb <mlateb@nuxly.com>
2026-07-20 15:23:50 +0200

    #21 [ADD] Create web_list_cumulative_balance module skeleton

---
+++ web_list_cumulative_balance/README.md
@@ -0,0 +1,5 @@
+# Nuxly Account List Cumulative Balance
+
+Odoo 19 module adding a live "Cumulative Balance" column on `account.move.line` list
+views, to help accountants reconcile entries manually (select lines and see whether
+they sum to zero).
---
+++ web_list_cumulative_balance/__manifest__.py
@@ -0,0 +1,13 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Account List Cumulative Balance",
+    "summary": "Live cumulative balance column on account move line list views",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "author": "Nuxly",
+    "license": "LGPL-3",
+    "depends": ["web", "account"],
+    "installable": True,
+    "application": False,
+}
