cb7ababfa0a93a2cce4a1679409c748149519baa
mlateb <mlateb@nuxly.com>
2026-07-21 09:16:04 +0200

    #21 [ADD] Add live cumulative balance widget and wire it to the Journal Items list

--- web_list_cumulative_balance/__manifest__.py
+++ web_list_cumulative_balance/__manifest__.py
@@ -3,11 +3,20 @@
 {
     "name": "Account List Cumulative Balance",
     "summary": "Live cumulative balance column on account move line list views",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "category": "Accounting",
     "author": "Nuxly",
     "license": "LGPL-3",
     "depends": ["web", "account"],
+    "data": [
+        "views/account_move_line_views.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "web_list_cumulative_balance/static/src/js/cumulative_balance_field.esm.js",
+            "web_list_cumulative_balance/static/src/xml/cumulative_balance_field.xml",
+        ],
+    },
     "installable": True,
     "application": False,
 }
