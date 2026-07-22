1cc1b1e3cb8576a6d21c8eb907d65bb1f3679623
mlateb <mlateb@nuxly.com>
2026-07-22 09:13:29 +0200

    #18 [FIX] Replace deprecated _sql_constraints with models.Constraint on hr.expense.ma.fuel.type

--- hr_expense_ma/__manifest__.py
+++ hr_expense_ma/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "HR Expense Mileage",
     "summary": "Manage mileage claims in employee expenses",
-    "version": "19.0.1.7.1",
+    "version": "19.0.1.7.2",
     "category": "Human Resources/Expenses",
     "website": "https://github.com/nuxly/nuxly-odoo-addons",
     "author": "Nuxly",
