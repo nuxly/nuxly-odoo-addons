b47143db5a1f9c7132c0e2be29a5b29e7b845a88
mlateb <mlateb@nuxly.com>
2026-07-27 13:56:25 +0200

    #26 [ADD] Add multi-company selection badge to the native company switcher button

--- web_multi_company_counter/__manifest__.py
+++ web_multi_company_counter/__manifest__.py
@@ -3,11 +3,17 @@
 {
     "name": "Multi-Company Selection Counter",
     "summary": "Systray badge showing how many companies are currently selected",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "category": "Extra Tools",
     "author": "Nuxly",
     "license": "LGPL-3",
     "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "web_multi_company_counter/static/src/xml/multi_company_counter.xml",
+            "web_multi_company_counter/static/src/scss/multi_company_counter.scss",
+        ],
+    },
     "installable": True,
     "application": False,
 }
