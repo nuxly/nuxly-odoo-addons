c7356631f9b27a2cef340c01ed30852936b9c309
mlateb <mlateb@nuxly.com>
2026-07-27 10:40:01 +0200

    #26 [ADD] Create web_multi_company_counter module skeleton

---
+++ web_multi_company_counter/README.md
@@ -0,0 +1,6 @@
+# Nuxly Multi-Company Selection Counter
+
+Odoo 19 module adding a small badge next to the company selector in the systray,
+showing how many companies are currently selected. Helps multi-company users
+notice when they have more than one company selected at once, to avoid getting
+confused by records shown from a company they did not expect.
---
+++ web_multi_company_counter/__manifest__.py
@@ -0,0 +1,13 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Multi-Company Selection Counter",
+    "summary": "Systray badge showing how many companies are currently selected",
+    "version": "19.0.1.0.0",
+    "category": "Extra Tools",
+    "author": "Nuxly",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "installable": True,
+    "application": False,
+}
